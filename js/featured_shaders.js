4c3f1c57d234294be2e7160f35d9935317864b37
Ed Mackey <elm19087@gmail.com>
2012-11-27 14:58:40 -0500

    Working on framework for featured shaders.

---
+++ js/featured_shaders.js
@@ -0,0 +1,23 @@
+var shader_showcase = {
+
+'categories':[
+{'id':1,'name':'Art &amp; Abstract','img':'','desc':''},
+{'id':2,'name':'Automata','img':'','desc':'http://en.wikipedia.org/wiki/Automata_theory'},
+{'id':3,'name':'Cellular','img':'','desc':''},
+{'id':4,'name':'Flags','img':'','desc':''},
+{'id':5,'name':'Fractals','img':'','desc':''},
+{'id':6,'name':'Game-like','img':'','desc':''},
+{'id':7,'name':'Other','img':'','desc':''},
+{'id':8,'name':'Raymarching &amp; Raytracing','img':'','desc':''},
+{'id':9,'name':'Terrain','img':'','desc':''},
+{'id':10,'name':'Text','img':'','desc':''},
+{'id':11,'name':'Tunnels','img':'','desc':''},
+{'id':12,'name':'Volumetric','img':'','desc':''}
+],
+
+'shaders':[
+{'e':'207.3','cat':[2],'name':'Conway\'s Game of Life','desc':'The classic game plays out in the backbuffer texture, using blue trails to highlight activity.'},
+{'e':'383.7','cat':[2],'name':'Al\'s Bugs','desc':'Bugs (in green) hunt for food (in blue).'},
+{'e':'677.0','cat':[3],'name':'Gems','desc':''}
+
+]};
---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(function () {
+    "use strict";
+
+    function onResize () {
+        $('.main').css('height', window.innerHeight + 'px');
+    }
+    onResize();
+    $(window).resize(onResize);
+
+    var iCat, lenCat = shader_showcase.categories.length, iShader, lenShader = shader_showcase.shaders.length;
+    var cat, shader, eleCat, eleShader, eleShaderCollection;
+
+    for (iCat = 0; iCat < lenCat; ++iCat) {
+        cat = shader_showcase.categories[iCat];
+        eleCat = document.createElement('div');
+        eleCat.className = 'category';
+        eleCat.innerHTML = '<h2>' + cat.name + '</h2>';
+        if (cat.desc) {
+	        eleCat.innerHTML += '<span class="description">' + cat.desc + '</span>';
+        }
+        eleShaderCollection = document.createElement('div');
+        eleShaderCollection.className = 'collection';
+        eleCat.appendChild(eleShaderCollection);
+        for (iShader = 0; iShader < lenShader; ++iShader) {
+            shader = shader_showcase.shaders[iShader];
+            if ($.inArray(cat.id, shader.cat) >= 0) {
+                eleShader = document.createElement('span');
+                //eleShader.className = 'shader';
+                eleShader.innerHTML = '<a class="shader" href="http://glsl.heroku.com/e#' + shader.e +
+                    '"><img src="shader_img/e' + shader.e + '.png" alt="' + shader.name + '" />' + shader.name + '</a> ';
+
+                eleShaderCollection.appendChild(eleShader);
+            }
+        }
+        $("#categories").append(eleCat);
+    }
+
+});
