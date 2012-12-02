dd05606c0683d0dad56fac26efc4ed0730d4ca8b
Ed Mackey <elm19087@gmail.com>
2012-12-02 16:18:23 -0500

    Added Carry the Torch

--- js/featured_shaders.js
+++ js/featured_shaders.js
@@ -15,7 +15,8 @@ var shader_showcase = {
 {"id":12,"name":"Volumetric","img":"","desc":""}],
 
 "shaders":
-[{"e":"207.3","ext":"png","cat":[2],"name":"Conway's Game of Life","desc":"The classic game plays out in the backbuffer texture, using blue trails to highlight activity."},
+[{"e":"91.27","ext":"png","cat":[6],"name":"Carry the Torch","desc":"Likely the first playable game on GLSL Sandbox.\n\nMove your mouse to the yellow start post at the bottom-right.  Get the green flame and carry it slowly and safely to the purple goal post in the upper-right.  If the screen turns blue, you win!!\n"},
+{"e":"207.3","ext":"png","cat":[2],"name":"Conway's Game of Life","desc":"The classic game plays out in the backbuffer texture, using blue trails to highlight activity."},
 {"e":"383.7","ext":"png","cat":[2],"name":"Al's Bugs","desc":"Bugs (in green) hunt for food (in blue)."},
 {"e":"677.0","ext":"png","cat":[3],"name":"Gems","desc":""},
 {"e":"1621.1","ext":"png","cat":[6],"name":"Half-a-Q*bert in GLSL","desc":"Q*bert will follow the mouse.\nJumping off the board will simply move you back to the top.\nThere's no win condition, but you can toggle each tile twice."}]
